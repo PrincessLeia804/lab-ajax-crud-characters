8f06951473021a341e8a1ae98758929dbd493253
Lea Model <lea.skywalker309@gmail.com>
2023-07-27 17:18:08 +0200

    Fetch all done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,53 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = axios.create({
+      baseUrl: 'http://localhost:8000/characters'
+    });
   }
 
-  getFullList () {
+  async getFullList() {
+
+   const all = await axios
+   .get(`http://localhost:8000/characters`)
+
+      if(all.status === 200){
+      console.log("Successfully fetched all");
+    } else {
+      console.log("COuldnt fetch")
+    }
+    return all
+  }
+
+
+  getOneRegister(id) {
+    return this.BASE_URL.get('/:id')
+
+    // axios
+    //   .get(`http://localhost:8000/characters/:id`)
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data))
+    //   .catch(err => {
+    //     console.log(err);
+    //     err.response.status === 404 ? alert(`The data doesn't exist.`) : alert('Server error! Sorry.');
+    //   })
 
   }
 
-  getOneRegister () {
-
+  createOneRegister(characterInfo) {
+    return this.BASE_URL.post('', characterInfo)
   }
 
-  createOneRegister () {
-
+  updateOneRegister(id) {
+    return this.BASE_URL.put('/:id', id)
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  async deleteOneRegister(id) {
+    const response = await axios.delete("http://localhost:8000/characters/:id")
+    if(response.status === 200){
+      console.log("Successfully deleted");
+    } else {
+      console.log("Character wasn't found")
+    }
+    return response .data
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,30 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async() => {
+      let allCharacters = await charactersAPI.getFullList()
+      
 
+      const parentContainer = document.querySelector(".characters-container");
+      parentContainer.innerHTML = ""; // Clear previous content
+
+      allCharacters.data.forEach((person) => {
+          // Create a new container for each character
+          let newContainer = document.createElement("div");
+          newContainer.setAttribute("class", "character-info");
+          newContainer.setAttribute("id", `${person.id}`);
+
+          // Populate the new container with character information
+          newContainer.innerHTML = `
+              <p class="name">${person.name}</p>
+              <p class="occupation">${person.occupation}</p>
+              <p class="cartoon">${person.cartoon}</p>
+              <p class="weapon">${person.weapon}</p>
+          `;
+
+          // Append the new container to the parent container
+          parentContainer.appendChild(newContainer);
+      })
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
